a1617fee17f772a02a9d6fbcb29801437badc34d
mayurgetmega <56244338+mayurgetmega@users.noreply.github.com>
2022-04-23 17:49:30 +0530

    rendering images as grid from api response

--- app/src/main/java/com/mayur/myimageapp/MainActivity.kt
+++ app/src/main/java/com/mayur/myimageapp/MainActivity.kt
@@ -3,11 +3,25 @@ package com.mayur.myimageapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mayur.myimageapp.data.SearchResultItem
+import com.mayur.myimageapp.data.SearchResults
 import com.mayur.myimageapp.ui.theme.MyImageAppTheme
+import com.skydoves.landscapist.glide.GlideImage
 
 class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<ImageViewModel> { ImageViewModel.Factory() }
@@ -21,6 +35,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ImageGridUI(viewModel: ImageViewModel) {
     val imagesResponse by viewModel.imagesResponse
@@ -28,7 +43,39 @@ fun ImageGridUI(viewModel: ImageViewModel) {
     LaunchedEffect(Unit) {
         viewModel.getImages("iphone")
     }
+
+    ImagesGridViewUI(imagesResponse = imagesResponse)
 }
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ImagesGridViewUI(imagesResponse: SearchResults?) {
+    imagesResponse?.results?.let { images ->
+        LazyVerticalGrid(cells = GridCells.Adaptive(100.dp)) {
+            items(images) { image ->
+                ImageItemUI(image)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageItemUI(image: SearchResultItem) {
+    Card(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(0.5.dp)
+            .clickable { },
+        elevation = 6.dp,
+        shape = RoundedCornerShape(0.dp),
+    ) {
+        GlideImage(
+            imageModel = image.urls.small
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
