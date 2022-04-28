1f740c4f05f3956615a847cac6ad5a933e2af17c
mayurgetmega <56244338+mayurgetmega@users.noreply.github.com>
2022-04-28 21:20:28 +0530

    code clean up

--- app/src/main/java/com/mayur/myimageapp/components/Palette.kt
+++ app/src/main/java/com/mayur/myimageapp/components/Palette.kt
@@ -1,34 +1,24 @@
 package com.mayur.myimageapp.components
 
-import android.util.Log
 import androidx.compose.ui.graphics.Color
 import androidx.palette.graphics.Palette
 
-// TODO clean palette code
 fun getBgColor(palette: Palette?): Color {
     return if (palette != null) {
-        Log.i("ImagesSearchUI", "vibrantSwatch ${palette!!.vibrantSwatch?.rgb}")
-        Log.i("ImagesSearchUI", "lightVibrantSwatch ${palette!!.lightVibrantSwatch?.rgb}")
         when {
-            palette!!.vibrantSwatch?.rgb != null -> Color(palette!!.vibrantSwatch!!.rgb)
-            palette!!.lightVibrantSwatch?.rgb != null -> Color(palette!!.lightVibrantSwatch!!.rgb)
-            else -> {
-                Log.i("ImagesSearchUI", "Color 1 white")
-                Color.White
-            }
+            palette.vibrantSwatch?.rgb != null -> Color(palette.vibrantSwatch!!.rgb)
+            palette.lightVibrantSwatch?.rgb != null -> Color(palette.lightVibrantSwatch!!.rgb)
+            else -> Color.White
         }
-    } else {
-        Log.i("ImagesSearchUI", "Color 2 white")
-        Color.White
-    }
+    } else Color.White
 }
 
 fun getTextColor(palette: Palette?): Color {
     return if (palette != null) {
         when {
-            palette!!.lightVibrantSwatch?.bodyTextColor != null -> Color(palette!!.lightVibrantSwatch!!.bodyTextColor)
-            palette!!.lightVibrantSwatch?.titleTextColor != null -> Color(palette!!.lightVibrantSwatch!!.titleTextColor)
-            palette!!.vibrantSwatch?.titleTextColor != null -> Color(palette!!.vibrantSwatch!!.titleTextColor)
+            palette.lightVibrantSwatch?.bodyTextColor != null -> Color(palette.lightVibrantSwatch!!.bodyTextColor)
+            palette.lightVibrantSwatch?.titleTextColor != null -> Color(palette.lightVibrantSwatch!!.titleTextColor)
+            palette.vibrantSwatch?.titleTextColor != null -> Color(palette.vibrantSwatch!!.titleTextColor)
             else -> Color.Black
         }
     } else Color.Black
